b1d41a59e66b1ced0c61e07165b6d3ac7b396765
Marc-André Malouin <mamalouin@spiria.com>
2022-11-20 22:23:39 -0500

    Day 01 solution + revised cleanup

--- run_solution.pl
+++ run_solution.pl
@@ -1,13 +1,14 @@
 use strict;
 use warnings;
 use File::chdir;
-use Benchmark qw(:all);
+use Time::HiRes qw(gettimeofday tv_interval);
 
 # Reading args
 my ($folder, $solution_number, @args) = @ARGV;
 
 # Running day folder solution
 $CWD = "./$folder";
-my $result = timeit(1, print `perl solution.pl $solution_number @args`);
-my $time = timestr($result);
+my $t0 = [gettimeofday];
+print `perl solution.pl $solution_number @args`;
+my $time = tv_interval($t0, [gettimeofday]);
 print "Done in $time\n";
